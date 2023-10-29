ad94ce88e717fe3190de7c707ab498cd514b19f1
dominikrollenmiller@gmail.com <frontsau@roboticaxolotlproject.com>
2023-10-29 15:41:48 +0100

    rly deleted some stuff :D

--- MarketOptimizer/src/main/java/com/example/marketoptimizer/database/PlayerManager.java
+++
@@ -1,9 +0,0 @@
-package com.example.marketoptimizer.database;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public class PlayerManager {
-}
